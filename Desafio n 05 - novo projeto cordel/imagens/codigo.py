e2c5ff6d201e144ca599af1b8a99a11d79d0a6e0
Samara b <156718996+Alvessamara@users.noreply.github.com>
2024-03-04 20:02:04 -0300

    tabelas

---
+++ Desafio n 05 - novo projeto cordel/imagens/codigo.py
@@ -0,0 +1,17 @@
+#Passo a passo do projeto
+#passo 1:Entrar no sistema da empresa
+#https://dlp.hashtagtreinamentos.com/python/intensivao/login
+#pip install pyautogui
+#passo 2: fazer login
+#passo 3 : importar a base de dados
+#passo 4: cadastrar 1 produto
+#passo 5:repetir o processo do cadastro até acabar a base de dados
+
+import pyautogui 
+#pyautogui.click -clicar em algum lugar da tela
+#pyautogui.write-escrever um texto
+#pyautogui.press- pressionar 1 tecla do teclado
+
+pyautogui.press("win")
+pyautogui.write("chorme")
+pyautogui.press("enter")
